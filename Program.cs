99801fba3bde20198acf132d9390a2515f3fc82b
Pedro Henrique Macedo <pedromacedofonseca@gmail.com>
2025-03-08 13:44:33 -0300

    Iniciando projeto

---
+++ Program.cs
@@ -0,0 +1,17 @@
+using MinimalAPI.DTOs;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Olá Pessoal!");
+
+app.MapPost("/login", (LoginDTO loginDTO) => {
+    if(loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456"){
+        return Results.Ok("Login com sucesso");
+    }
+    else
+        return Results.Unauthorized();
+});
+
+app.Run();
+
